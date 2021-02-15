2bc8499d6709cd4c7d881a7a7bc0aee899ea6789
java_men <speleo04@yandex.ru>
2021-02-15 18:00:48 +0300

    Initial commit

---
+++ src/main/java/io/CloudApplication.java
@@ -0,0 +1,20 @@
+package io;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CloudApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("cloud_term.fxml"));
+        primaryStage.setScene(new Scene(parent)); //Scene  некоторый фронт ожидания действий клиента
+        primaryStage.setTitle("Clo");
+        //primaryStage.setOnCloseRequest(); переопределение закрывашки
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
